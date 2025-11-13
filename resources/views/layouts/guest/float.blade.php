11ce735126587bbde8495cb3be0ec0740004e1c9
nisa <yuannisa24si@mahasiswa.pcr.ac.id>
2025-11-13 10:22:02 +0700

    ubah floating button

--- resources/views/layouts/guest/float.blade.php
+++ resources/views/layouts/guest/float.blade.php
@@ -1,8 +1,13 @@
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i
-            class="bi bi-arrow-up"></i></a>
+<!-- Tombol Back to Top -->
+<a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"
+    style="position: fixed; bottom: 80px; right: 20px; z-index: 1000;">
+    <i class="bi bi-arrow-up"></i>
+</a>
 
-    <!-- Whatsapp -->
-    <a href="https://wa.me/6281234567890" target="_blank"
-        class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top" style="bottom: 10%">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" width="100%"
-            height="100%">
+<!-- Tombol WhatsApp -->
+<a href="https://wa.me/6281234567890" target="_blank"
+    class="btn btn-lg btn-success btn-lg-square rounded-circle"
+    style="position: fixed; bottom: 20px; right: 20px; z-index: 9999;">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+        alt="WhatsApp" width="25" height="25">
+</a>
