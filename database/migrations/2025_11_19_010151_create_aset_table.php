1416dd8502d5f7cc67618c1f0d09394a81b2f252
elin143 <eveline24si@mahasiswa.pcr.ac.id>
2025-11-19 18:33:01 +0700

    perubahan table

--- database/migrations/2025_11_19_010151_create_aset_table.php
+++ database/migrations/2025_11_19_010151_create_aset_table.php
@@ -5,26 +5,27 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-public function up(): void
-{
-Schema::create('aset', function (Blueprint $table) {
-$table->id('aset_id');
-$table->unsignedBigInteger('kategori_id');
-$table->string('kode_aset')->unique();
-$table->string('nama_aset');
-$table->date('tgl_perolehan');
-$table->decimal('nilai_perolehan', 15, 2);
-$table->string('kondisi');
-$table->timestamps();
+    public function up(): void
+    {
+        Schema::create('aset', function (Blueprint $table) {
+            $table->id('aset_id');
+            $table->unsignedBigInteger('kategori_id');
+            $table->string('kode_aset')->unique();
+            $table->string('nama_aset');
+            $table->date('tgl_perolehan');
+            $table->decimal('nilai_perolehan', 15, 2);
+            $table->string('kondisi');
+            $table->timestamps();
 
+            $table->foreign('kategori_id')
+                ->references('id')
+                ->on('kategori_aset')
+                ->onDelete('cascade');
+        });
+    }
 
-$table->foreign('kategori_id')->references('id')->on('kategori_aset')->onDelete('cascade');
-});
-}
-
-
-public function down(): void
-{
-Schema::dropIfExists('aset');
-}
+    public function down(): void
+    {
+        Schema::dropIfExists('aset');
+    }
 };
